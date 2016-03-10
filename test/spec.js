36978f141e0f392e1fe79dfe9a427a802f11d37d
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-03-10 10:25:04 +0100

    Aligned

--- test/spec.js
+++ test/spec.js
@@ -1,40 +1,34 @@
 var assert  = require('assert')
+var clone   = require('clone')
 var zsf     = require('@zombiec0rn/zombie-service-format')
 var scale   = require('@zombiec0rn/zombie-service-scale')
-var clone   = require('clone')
 var cdiff   = require('../index')
-
-var current = [
-	clone(example),
-	clone(example)
-]
-var wanted  = [
-	clone(example),
-	clone(example)
-]
-current[1].id = 'remove'
-wanted[1].id = 'new'
 
 describe('cccd-diff', function() {
 
-	it('returns an object with keep, add and remove', function() {
-		var diff = cdiff(current, wanted)
-		assert(typeof diff == 'object')
-		assert(diff.keep.length == 1)
-		assert(diff.add.length == 1)
-		assert(diff.remove.length == 1)
-	})
+  it('returns an object with keep, add and remove', function() {
+    var current = zsf.random(2)
+    var wanted = zsf.random(1)
+    wanted.push(current[0])
+    var diff = cdiff(current, wanted)
+    assert(typeof diff == 'object')
+    assert(diff.keep.length == 1)
+    assert(diff.add.length == 1)
+    assert(diff.remove.length == 1)
+  })
 
 	it('works with scales', function() {
-		var _wanted = clone(wanted)
-		_wanted[1].scale = 5
-		var upscaled = cs.up(_wanted)
+    var current = zsf.random(2)
+    var wanted = zsf.random(1)
+    wanted.push(current[0])
+		wanted[1].scale = 5
+		var upscaled = scale.up(wanted)
 		var diff = cdiff(current, upscaled)
 		assert(typeof diff == 'object')
 		assert(diff.keep.length == 1)
 		assert(diff.add.length == 5)
 		assert(diff.remove.length == 1)
-		var downscaled = cs.down(upscaled)
+		var downscaled = scale.down(upscaled)
 		var diff = cdiff(upscaled, downscaled)
 		assert(typeof diff == 'object')
 		assert(diff.keep.length == 2)
@@ -48,25 +42,26 @@ describe('cccd-diff', function() {
 		}
 	})
 
-    it('looks at all the properties', function() {
-        var _current = [clone(example)]
-        var _wanted  = [clone(example)]; _wanted[0].image = _wanted[0].image+':1.2.3'
-        var diff     = cdiff(_current, _wanted)
-        assert(diff.add.length == 1)
-        assert(diff.keep.length == 0)
-        assert(diff.remove.length == 1)
-    })
+  it('looks at all the properties', function() {
+    var current = zsf.random(1)
+    var wanted = clone(current) 
+    wanted[0].image = wanted[0].image+':1.2.3'
+    var diff     = cdiff(current, wanted)
+    assert(diff.add.length == 1)
+    assert(diff.keep.length == 0)
+    assert(diff.remove.length == 1)
+  })
 
-    it('can handle properties in random order', function() {
-        var _current = [clone(example)]
-        var _wanted  = [clone(example)]
-        var env = _current[0].env
-        delete _current[0].env
-        _current[0].env = env
-        var diff = cdiff(_current, _wanted)
-        assert(diff.add.length == 0)
-        assert(diff.keep.length == 1)
-        assert(diff.remove.length == 0)
-    })
+  it('can handle properties in random order', function() {
+    var current = zsf.random(1)
+    var wanted = clone(current) 
+    var env = current[0].env
+    delete current[0].env
+    current[0].env = env
+    var diff = cdiff(current, wanted)
+    assert(diff.add.length == 0)
+    assert(diff.keep.length == 1)
+    assert(diff.remove.length == 0)
+  })
 
 })
